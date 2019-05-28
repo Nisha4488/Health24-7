49b14c39361e080e947cff15dd951d96e4016726
Nisha Bharti <me.nishabharti@gmail.com>
2019-05-28 00:35:40 -0400

    added lint-staged

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import Header from     "./components/Header";
-import Main from './components/Main';
+import Main from    './components/Main';
 import PrettierTest from './components/PrettierTest';
 
 function App() {
   return (
     <div className="App">
       <PrettierTest          />
-      <Header />
+      <Header      />
       <Main />
     </div>
   );

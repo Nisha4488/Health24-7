d6108f51ac6ec01bfcde2fc4a7a9b34bb5d47a8e
Nisha Bharti <me.nishabharti@gmail.com>
2019-05-28 00:40:12 -0400

    added husky configuration in package.json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from     "./components/Header";
-import Main from    './components/Main';
+import Header from './components/Header';
+import Main from './components/Main';
 import PrettierTest from './components/PrettierTest';
 
 function App() {
   return (
     <div className="App">
-      <PrettierTest          />
-      <Header      />
+      <PrettierTest />
+      <Header />
       <Main />
     </div>
   );
